dbd3bca661a487df7f014cd61f41f7010616cdb9
Preston Zhang <preston0769@gmail.com>
2017-01-19 13:10:46 +1100

    Add UI, Learning RelativeLayout LinearLayout

--- Basic_UI_and_Event/app/src/main/java/com/example/yuantao/basic_ui_and_event/MainActivity.java
+++ Basic_UI_and_Event/app/src/main/java/com/example/yuantao/basic_ui_and_event/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.yuantao.basic_ui_and_event;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton(){
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent broswerIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com.au"));
+                startActivity(broswerIntent);
+            }
+        });
     }
 }
